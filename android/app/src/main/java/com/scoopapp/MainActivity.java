08063610ccc90bdc71feddcad71c093ca7e9afda
alvinyho <alvinyongho@gmail.com>
2017-04-16 01:18:18 -0700

    login works with android

--- android/app/src/main/java/com/scoopapp/MainActivity.java
+++ android/app/src/main/java/com/scoopapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.scoopapp;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +13,11 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "ScoopApp";
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
 }
--- android/app/src/main/java/com/scoopapp/MainApplication.java
+++ android/app/src/main/java/com/scoopapp/MainApplication.java
@@ -9,10 +9,19 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -24,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage()
+              new FBSDKPackage(mCallbackManager)
+
       );
     }
   };
