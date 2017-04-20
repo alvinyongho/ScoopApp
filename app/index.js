fd7762b12c20620397c162f81e198549a4a31c17
alvinyho <alvinyongho@gmail.com>
2017-04-19 19:21:46 -0700

    redux login handling

--- app/index.js
+++ app/index.js
@@ -9,23 +9,61 @@ import {
   Button,
 } from 'react-native';
 
+
+import { createStore } from 'redux';
+
 import { StackNavigator } from 'react-navigation';
-import Login from './components/Login/index.js'
+// import Login from './components/Login/index.js'
 import FeedList from './components/FeedList/index.js'
+
+
+const auth = (state = {}, action) => {
+  switch (action.type) {
+    case 'LOGIN_REQUEST':
+      return [
+        ...state,
+        {
+          loginStatusMessage: 'Logging in...',
+        }
+      ]
+    case 'LOGIN_SUCCESS':
+      return [
+        ...state,
+        {
+          loginStatusMessage: 'Login successful',
+        }
+      ]
+    case 'LOGIN_FAIL':
+      return[
+        ...state,
+        {
+          loginStatusMessage: 'Login failed'
+        }
+      ]
+  }
+}
+
+const store = createStore(auth);
+
+
+const Login = ({ loginMessage }) => (
+  <h1>{loginMessage}</h1>
+);
 
 
 export default class AppEntry extends Component {
   constructor(props) {
     super (props);
-    this.state = {      
+    this.state = {
     };
   }
 
-
-
   render() {
     return(
-      <Login />
+      <View>
+        <Text> Testing login </Text>
+        <Login loginMessage={store.getState()} />
+      </View>
     )
   }
 }
