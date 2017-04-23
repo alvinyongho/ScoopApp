fc34abdbe73095b101ce41e5d2beb9d2317c864a
alvinyho <alvinyongho@gmail.com>
2017-04-22 22:55:16 -0700

    match found state updates and retrieval using the prop variable that was mapped from the state attribute

---
+++ app/containers/Home.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+import { connect } from 'react-redux';
+import {
+  ScrollView,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  StyleSheet,
+} from 'react-native';
+
+class Home extends Component{
+  searchMatches() {
+    // due to destruct in app container <Home {...this.props} all the actions
+    // from the AppContainer get passed into the Home container
+    this.props.fetchMatches(
+      match_attributes =
+        {
+          'attribute1': 'criteria1',
+          'atrribute2': 'criteria2',
+        }
+    )
+  }
+
+  matches(){
+    // if(this.props.foundMatches){
+        return Object.keys(this.props.foundMatches)
+          .map( key => this.props.foundMatches[key])
+    // }
+  }
+
+
+  render(){
+    console.log(this.matches())
+    return (
+      <View style={{margin: 20}}>
+        <View>
+          <TouchableHighlight onPress={() => this.searchMatches() }>
+            <Text>Fetch Matches</Text>
+          </TouchableHighlight>
+        </View>
+        <ScrollView>
+
+        </ScrollView>
+      </View>
+
+    );
+  }
+}
+
+// Match state to props which allows us to access actions
+function mapStateToProps(state){
+  return {
+    foundMatches: state.foundMatches
+  }
+}
+
+
+// Connects the state variables to the property variables within
+// the home class
+export default connect(mapStateToProps)(Home);
