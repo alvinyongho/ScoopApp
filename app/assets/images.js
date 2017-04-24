104c148e1b3edbcf738a1f61e42483af44d77149
alvinyho <alvinyongho@gmail.com>
2017-04-24 11:14:07 -0700

    styling updates

--- app/assets/images.js
+++ app/assets/images.js
@@ -3,7 +3,10 @@ const images = {
 	welcome2: require('./img/WelcomePages/welcome-2.png'),
 	welcome3: require('./img/WelcomePages/welcome-3.png'),
   welcome4: require('./img/WelcomePages/welcome-4.png'),
+	facebookWhiteCircularLogo: require('./img/WelcomePages/facebook-whitecircular-logo.png'),
 	signInButton: require('./img/Auth/Facebook-sign-in.png'),
+	splashImg: require('./img/Splash/splash-img.png'),
+	scoopLogo: require('./img/Logo/scoop-icon.png'),
 };
 
 export default images;
--- app/components/WelcomePages/index.js
+++ app/components/WelcomePages/index.js
@@ -4,6 +4,7 @@ import {
   Text,
   StyleSheet,
   Image,
+  TouchableHighlight,
 } from 'react-native';
 
 import Swiper from 'react-native-swiper';
@@ -17,9 +18,74 @@ export default class WelcomePages extends Component {
   render(){
     return(
       <Swiper style={styles.wrapper} showsButtons={false} loop={false}>
-        <View>
-          <Image source={images.welcome1} style={styles.pageImage}/>
+
+        {/* Login page */}
+        <View style={{flex: 1, margin:40, marginBottom:60}}>
+          <View style={{flex: 1.25, backgroundColor: 'powderblue', justifyContent: 'center', alignItems: 'center'}}>
+            <Image source={images.splashImg} style={{width:100, height:80, resizeMode: 'contain',}} />
+            <Image source={images.scoopLogo} style={{width:150, height:80, resizeMode: 'contain',}} />
+          </View>
+          <View style={{flex: 1, backgroundColor: 'skyblue'}}>
+            <Text style={{padding: 25, fontSize:20, textAlign:'center'}}>
+              Let us do the leg work, date someone with mutual interests by
+              connecting your favorite apps to Scoop
+            </Text>
+          </View>
+          <View style={{flex: 1, backgroundColor: 'steelblue'}}>
+
+            <View style={{flex: 1, backgroundColor: 'steelblue'}}>
+              <Text style={{padding:20, fontSize:11, textAlign:'center'}}>
+                By logging in, you agree to our terms of use and private policy.
+              </Text>
+            </View>
+
+            <View style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+              <View style={{
+                width: 60,
+                height: 40,
+                justifyContent: 'center', alignItems: 'center',
+                backgroundColor:'#68a0cf',
+                borderRadius:3,
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+                backgroundColor: 'powderblue'}}>
+                <Image source={images.facebookWhiteCircularLogo} style={{width:30, height:30}} />
+              </View>
+              <Link to="/login">
+              <View style={{
+                padding: 10,
+                width: 180,
+                height: 40,
+                backgroundColor:'#68a0cf',
+                borderRadius:3,
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+                borderWidth: 0,
+                borderColor: '#fff'}}>
+                <Text style={{fontSize:18, textAlign:'center', color:'white'}}>Sign in to Facebook</Text>
+              </View>
+              </Link>
+            </View>
+
+            <View style={{flex: 1, backgroundColor: 'steelblue'}}>
+              <Text style={{margin:10, fontSize:11, textAlign:'center'}}>
+                Scoops will never post to your Facebook Account without your
+                permission.
+              </Text>
+            </View>
+
+          </View>
+
+
+
         </View>
+        {/* End of Login Page */}
+
         <View>
           <Image source={images.welcome2} style={styles.pageImage}/>
         </View>
@@ -29,24 +95,7 @@ export default class WelcomePages extends Component {
         <View>
           <Image source={images.welcome4} style={styles.pageImage}/>
         </View>
-        <View style={styles.login}>
-          <Text style={{fontSize:30}}>
-            Let us do the leg work, date someone with mutual interests by
-            connecting your favorite apps to Scoop
-          </Text>
-          <Text>
-            By logging in, you agree to our terms of use and private policy.
-          </Text>
 
-          <Link to="/login">
-            <Image source={images.signInButton} resizeMode='contain' style={{width: 300}}/>
-          </Link>
-
-          <Text>
-            Scoops will never post to your Facebook Account without your
-            permission.
-          </Text>
-        </View>
       </Swiper>
     )
   }
