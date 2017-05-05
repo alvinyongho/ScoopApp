57096846a1a1a4b8ee5aa2c3667db27fecfa9e96
alvinyho <alvinyongho@gmail.com>
2017-05-04 17:52:54 -0700

    view component

--- app/components/PersonalProfile/PanningRectExample.js
+++ app/components/PersonalProfile/PanningRectExample.js
@@ -18,96 +18,80 @@ const screenHeight = Dimensions.get('window').height;
 var ALBUM_WIDTH = 80;
 var ALBUM_HEIGHT = 60;
 
+var MARGIN = 20;
+
+
+boxHeight = screenWidth/3 - 35
+boxWidth = screenWidth/3 - 15
+
 
 export default class PanningRectExample extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      pan: new Animated.ValueXY(),
-      scale: new Animated.Value(1)
+    this._width = boxWidth;
+    this._height = boxHeight
 
-    // album is an object with a property that is a function named setNativeProps
-    // is by default null until an object is assigned to it
-    // setNativeProps returns void
-    // album: (null : ?{ setNativeProps(props: Object): void }),
+
+    // the last item set as selected
+    this.state = {
+        selected: 5,
+        albumPictures: [
+          {
+            key: 0,
+            backgroundColor: 'red'
+          },
+          {
+            key: 1,
+            backgroundColor: 'orange'
+          },
+          {
+            key: 2,
+            backgroundColor: 'yellow'
+          },
+          {
+            key: 3,
+            backgroundColor: 'green'
+          },
+          {
+            key: 4,
+            backgroundColor: 'blue'
+          },
+          {
+            key: 5,
+            backgroundColor: 'purple'
+          }
+        ]
     }
   }
 
-
   componentWillMount(){
     // Handle the pannign responders
-
     this._panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture: () => true,
-      onMoveShouldSetPanResponderCapture: () => true,
-
-      onPanResponderGrant: (e, gestureState) => {
-        // Set the initial value to the current state
-        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
-        this.state.pan.setValue({x: 0, y: 0});
-
-        Animated.sequence([
-          Animated.spring(
-            this.state.scale,
-            { toValue: 1.1, friction: 3 }
-          ),
-
-        ]).start();
-      },
-
-      // When we drag/pan the object, set the delate to the states pan position
-      onPanResponderMove: Animated.event([
-        null, {dx: this.state.pan.x, dy: this.state.pan.y},
-      ]),
-
-      onPanResponderRelease: (e, {vx, vy}) => {
-        // Flatten the offset to avoid erratic behavior
-        this.state.pan.flattenOffset();
-
-        // Animated.sequence([
-          Animated.spring(
-            this.state.scale,
-            { toValue: 1, friction: 3 }
-          ).start();
-          Animated.spring(            //Step 1
-              this.state.pan,         //Step 2
-              {toValue:{x:0,y:0}}     //Step 3
-          ).start();
-        // ]).start();
-      }
     });
-
-
   }
+
+  // When the layout of the view gets loaded we set an async function that will
+  // bind the DropZoneLayout after the styles all get loaded
 
   render(){
 
-
+    const picturesBoxes = this.state.albumPictures.map((elem, index) => {
+      let top = Math.floor(index/3) * this._height;
+      let left = (index % 3) * this._width;
+      return (
+        <View ref={"pictureBox" + index} key={elem.key} style={[styles.pictureBox, {top, left}]} >
+          <View style={[styles.pictureBoxContainer, {backgroundColor:elem.backgroundColor}]}>
+            {/* TODO: add image here */}
+            <Text> Content Here </Text>
+          </View>
+        </View>
+      );
+    })
     return (
       <View
-        style={styles.container}>
-        <View style = {styles.dropZone}>
-          <Text> DRAG TO HERE </Text>
-        </View>
-
-        { this.renderDraggable() }
-      </View>
-    );
-  }
-
-  renderDraggable(){
-    let { pan, scale } = this.state;
-    let [translateX, translateY] = [pan.x, pan.y];
-    let rotate = '0deg';
-    let albumStyle = {transform: [{translateX}, {translateY}, {rotate}, {scale}]};
-
-    return (
-      <View style={styles.draggableContainer}>
-      <Animated.View
-        style={[albumStyle, styles.album]}
-        {...this._panResponder.panHandlers}
-      />
+        style={styles.smallAlbumsContainer}>
+          {picturesBoxes}
       </View>
     );
   }
@@ -118,30 +102,30 @@ export default class PanningRectExample extends React.Component {
 }
 
 
-let Window = Dimensions.get('window');
+
 
 var styles = StyleSheet.create({
-  album: {
-    backgroundColor: 'green',
-    height: 80,
-    width: 100,
-    top: 240,
+  smallAlbumsContainer:{
+    width: screenWidth,
+    backgroundColor: 'skyblue',
+    margin: MARGIN
   },
 
-  draggableContainer:{
-    position: 'absolute',
-
+  pictureBoxContainer:{
+    alignItems:"center",
+    justifyContent:"center",
+    width: screenWidth/3 - 15,
+    height:screenWidth/3 - 35,
   },
 
-  dropZone: {
-    height: 200,
-    margin:20,
-    backgroundColor:'#2c3e50'
-  },
 
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
+  pictureBox:{
+    width: screenWidth/3 - 15,
+    height: screenWidth/3 - 35,
+    backgroundColor:"#fff",
+    position:"absolute",
+    left:0,
+    top:0,
   },
 
 });
