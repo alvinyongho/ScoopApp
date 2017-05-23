31d057086de35dd428c057b6fa49191179a933e2
Allen Fang <allenfang08@gmail.com>
2017-05-22 20:35:39 -0700

    datetime formatting

--- app/containers/Messenger/ChatDetail.js
+++ app/containers/Messenger/ChatDetail.js
@@ -9,14 +9,23 @@ import {
   StyleSheet,
   Button,
   Navigator,
+  Dimensions
 } from 'react-native';
 
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export default class ChatDetail extends Component{
 
   render(){
     return(
-      <View><Text>hello world!</Text></View>
+      <View style={{
+        flex: 1,
+        backgroundColor: 'white'}}>
+        <View style={{height: 30, marginTop: 30, marginBottom: 30}}>
+          <Text style={{textAlign:'center', color: '#666666'}}>April 28, 2017  8:10 PM</Text>
+        </View>
+      </View>
     )
   }
 }
