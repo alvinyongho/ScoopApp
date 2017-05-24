7c9bb43ef7deddd22392442bcc9e64186ee6bda3
alvinyho <alvinyongho@gmail.com>
2017-05-24 13:09:57 -0700

    added text input and style

--- app/containers/Messenger/ChatDetail.js
+++ app/containers/Messenger/ChatDetail.js
@@ -7,14 +7,14 @@ import {
   Image,
   TouchableHighlight,
   StyleSheet,
-  Button,
   Navigator,
   Dimensions,
   TextInput,
-  Keyboard
+  Animated
 } from 'react-native';
 
 import MessageBubble from './MessageBubble';
+import Button from 'react-native-button'
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -39,24 +39,38 @@ export default class ChatDetail extends Component{
     return(
 
 
-      <View style={{flex:1}}>
-
+      <Animated.View style={{flex:1}}>
           <ScrollView style={styles.container}>
             <View style={styles.datetimeContainer}>
               <Text style={styles.datetimeText}>April 28, 2017  8:10 PM</Text>
             </View>
 
-
-
-
-
             <MessageBubble />
-
-
 
           </ScrollView>
 
-      </View>
+          <View style={{backgroundColor:"#F0F0F0", flexDirection: 'row'}}>
+          <TextInput placeholder={'Type message'}
+            style={{
+              flex: .82,
+              borderRadius: 5,
+            borderWidth: 1,
+            fontFamily: 'Avenir-Light',
+            margin: 7,
+            height: 44,
+            backgroundColor: 'white',
+            borderColor: '#D1D1D1',
+            paddingHorizontal: 10,}} />
+
+            <View style={{flex:.18, margin: 7, alignItems: 'center', justifyContent: 'center'}}>
+          <Button>
+            <Text style={{fontSize: 20, fontFamily: 'Avenir', color:'#A2DDEE', fontWeight:'bold',}}>Send </Text>
+          </Button>
+
+          </View>
+          </View>
+
+      </Animated.View>
 
     )
   }
