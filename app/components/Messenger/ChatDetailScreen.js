d51be615f8577398dc3e2c7797e6fbd703ab94d6
alvinyho <alvinyongho@gmail.com>
2017-05-24 13:56:25 -0700

    added top right navigation button for chat detail page

--- app/components/Messenger/ChatDetailScreen.js
+++ app/components/Messenger/ChatDetailScreen.js
@@ -27,7 +27,17 @@ class ChatDetailScreen extends Component{
   static navigationOptions = ({navigation}) => ({
     title: 'Scoop',
     headerLeft: <Button onPress={() => navigation.goBack()}>
-                         <Text style={{marginLeft: 20, fontFamily:'Avenir-Light', fontSize: 18, color:'white'}}>Back</Text>
+                         <Text style={{marginLeft: 15, fontFamily:'Avenir-Light', fontSize: 18, color:'white'}}>Back</Text>
+                 </Button>,
+
+    headerRight: <Button onPress={() => console.log('report and ignore button')}>
+
+                    <View style={{marginRight: 10, flexDirection: 'row', justifyContent: 'center'}}>
+                         <View style={{backgroundColor:'white', height: 6, width: 6, borderRadius: 6/2}}/>
+                         <View style={{backgroundColor:'white', height: 6, width: 6, borderRadius: 6/2, marginLeft: 4}}/>
+                         <View style={{backgroundColor:'white', height: 6, width: 6, borderRadius: 6/2, marginLeft: 4}}/>
+
+                    </View>
                  </Button>,
     headerStyle: {backgroundColor: '#54C9EC',},
     headerTitleStyle: {color: 'white', alignSelf:'center'}
--- app/containers/Messenger/ChatDetail.js
+++ app/containers/Messenger/ChatDetail.js
@@ -10,6 +10,7 @@ import {
   Navigator,
   Dimensions,
   TextInput,
+  Keyboard,
   Animated
 } from 'react-native';
 
@@ -19,55 +20,66 @@ import Button from 'react-native-button'
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
+
+// KEYBOARD_HEIGHT:
 export default class ChatDetail extends Component{
+
+  constructor(props){
+    super(props)
+    this.keyboardHeight = new Animated.Value(0);
+  }
+
   componentWillMount () {
-    // this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow',
-    //   this._keyboardDidShow)
-    // this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide)
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow',
+      this._keyboardDidShow)
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide)
 
   }
   //
-  // _keyboardDidHide(){
-  //   alert('Keyboard shown')
-  // }
-  //
-  // _keyboardDidShow(){
-  //   alert('Keyboard hidden')
-  // }
+  _keyboardDidHide = (event) => {
+    // alert('Keyboard shown')
+    Animated.timing(this.keyboardHeight,{
+      toValue: 0,
+      duration: event.duration,
+    }).start();
+
+  }
+
+  _keyboardDidShow = (event) => {
+    // alert('Keyboard hidden')
+    Animated.timing(this.keyboardHeight,{
+      toValue: event.endCoordinates.height,
+      duration: event.duration,
+    }).start();
+  };
+
+  handleSend = () => {
+    Keyboard.dismiss()
+  }
 
   render(){
     return(
 
-
-      <Animated.View style={{flex:1}}>
+      <Animated.View style={[styles.container, {paddingBottom: this.keyboardHeight}]}>
           <ScrollView style={styles.container}>
             <View style={styles.datetimeContainer}>
               <Text style={styles.datetimeText}>April 28, 2017  8:10 PM</Text>
             </View>
-
             <MessageBubble />
 
           </ScrollView>
 
+          <View style={{backgroundColor:'#D1D1D1', height: 1}} />
           <View style={{backgroundColor:"#F0F0F0", flexDirection: 'row'}}>
           <TextInput placeholder={'Type message'}
-            style={{
-              flex: .82,
-              borderRadius: 5,
-            borderWidth: 1,
-            fontFamily: 'Avenir-Light',
-            margin: 7,
-            height: 44,
-            backgroundColor: 'white',
-            borderColor: '#D1D1D1',
-            paddingHorizontal: 10,}} />
+            style={styles.inputTextBox} />
+
 
             <View style={{flex:.18, margin: 7, alignItems: 'center', justifyContent: 'center'}}>
-          <Button>
-            <Text style={{fontSize: 20, fontFamily: 'Avenir', color:'#A2DDEE', fontWeight:'bold',}}>Send </Text>
-          </Button>
-
-          </View>
+              <Button onPress={this.handleSend}>
+                <Text style={{fontSize: 20, fontFamily: 'Avenir', color:'#A2DDEE', fontWeight:'bold',}}>Send </Text>
+              </Button>
+            </View>
           </View>
 
       </Animated.View>
@@ -81,6 +93,17 @@ var styles = StyleSheet.create({
   container:{
     flex: 1,
     backgroundColor: 'white'
+  },
+  inputTextBox:{
+    flex: .82,
+    borderRadius: 5,
+    borderWidth: 1,
+    fontFamily: 'Avenir-Light',
+    margin: 7,
+    height: 44,
+    backgroundColor: 'white',
+    borderColor: '#D1D1D1',
+    paddingHorizontal: 10
   },
   datetimeContainer: {
     height: 30,
