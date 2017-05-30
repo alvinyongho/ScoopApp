9920b9c814d438d7fe0852a5485e53c817b13beb
alvinyho <alvinyongho@gmail.com>
2017-05-30 00:11:38 -0700

    updated plist with scoop fb app key

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -28,8 +28,8 @@ export function getScoopUserIdAndToken() {
   return (dispatch, getState) => {
     console.log('going into getScoopUserIdAndToken function')
     console.log(getState().userProfile.facebookProfile.id)
-    // let fbId = getState().userProfile.facebookProfile.id
-    let fbId = 10211414919833392
+    let fbId = getState().userProfile.facebookProfile.id
+    // let fbId = 10211414919833392
     getUserIdAndToken(fbId).then((userCredentials) => {
       console.log(userCredentials)
       dispatch(getToken(userCredentials.userInfo.userId, userCredentials.userInfo.userToken));
