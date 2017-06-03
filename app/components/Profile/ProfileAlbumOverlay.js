19b15eac0f254435e65501742ce2afe084ac0eaa
alvinyho <alvinyongho@gmail.com>
2017-06-02 20:21:46 -0700

    completed album image overlay

--- app/components/Profile/ProfileAlbumOverlay.js
+++ app/components/Profile/ProfileAlbumOverlay.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-import {View, Dimensions, TouchableHighlight} from 'react-native'
+import {View, Dimensions, TouchableHighlight, Image} from 'react-native'
 import Swiper from './react-native-page-swiper'
 
 
@@ -15,11 +15,28 @@ export default class ProfileAlbumOverlay extends Component{
     this.props.closeAlbum()
   }
 
-  profilePictures = (pics) => {
+  componentDidMount(){
+    console.log('PROPS')
     console.log(this.props)
-
-    return <View/>
   }
+
+  profilePictures = () => {
+
+    if(this.props.overlayImages.length){
+      return pictureViews = this.props.overlayImages.map((imgsource, index) =>{
+          if(imgsource){
+            console.log(imgsource)
+            return(
+              <Image key={'albumPicture'+index} source={{uri:imgsource}} style={{backgroundColor: 'white', top: 100, height: 400, width: 100}}/>
+            )
+          }
+      })
+
+    }
+    return <View />
+
+  }
+
 
   render(){
     viewWidth =  Dimensions.get('window').width
@@ -27,22 +44,26 @@ export default class ProfileAlbumOverlay extends Component{
 
     return(
       <View style={{flex: 1, position: 'absolute', backgroundColor: 'black', flex: 1, height: viewHeight, width:viewWidth }}>
-        <TouchableHighlight onPress={()=>this.closeAlbum()}>
-          <View style={{position: 'absolute', width:28, height:28, top:33, left:10, backgroundColor: 'white', borderRadius: 14}}>
-      {/*      <Swiper
-              showsButtons={true}
-              loop={false}
-              onShortPress={()=>{}}
-              onDragRelease={()=>{}}
-              onDragStart={()=>{}}
-            >
-              {this.profilePictures()}
-            </Swiper>*/}
-
-          </View>
-        </TouchableHighlight>
+      <View style={{flex: 1, position: 'absolute', backgroundColor: 'black', flex: 1, height: viewHeight, width:viewWidth }} >
 
 
+
+          <Swiper
+            showsButtons={true}
+            loop={false}
+            onShortPress={()=>{}}
+            onDragRelease={()=>{}}
+            onDragStart={()=>{}}
+          >
+            {this.profilePictures()}
+          </Swiper>
+
+      </View>
+
+      <TouchableHighlight onPress={()=>this.closeAlbum()}>
+        <View style={{position: 'absolute', width:28, height:28, top:33, left:10, backgroundColor: 'white', borderRadius: 14, zIndex:1}}>
+        </View>
+      </TouchableHighlight>
       </View>
 
     );
