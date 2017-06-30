fa9c32d0c4911dde95d066395e18665f8e058898
Allen Fang <allenfang08@gmail.com>
2017-06-30 11:24:13 -0700

    added service selection logos

---
+++ app/assets/img/ServiceSelection/Background/imageFormatter.py
@@ -0,0 +1,35 @@
+import os
+
+def get_filepaths(directory):
+	"""
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+	file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+	for root, directories, files in os.walk(directory):
+		for filename in files:
+			# Join the two strings in order to form the full filepath.
+			filename, file_extension = os.path.splitext(filename)
+			# print filename
+			# print file_extension
+
+
+			# airbnb_bgLogo: require('./img/ServiceSelection/Background/AIRBNB.png'),
+
+			if file_extension == '.png':
+				filename_lower = filename.lower()
+				parsed_result = ("{}_bgLogo: require('./img/ServiceSelection/Background/{}.png'),".format(filename_lower, filename))
+				file_paths.append(parsed_result)
+
+
+			# filepath = os.path.join(root, filename)
+			# file_paths.append(filepath)  # Add it to the list.
+
+	return file_paths  # Self-explanatory.
+
+full_file_paths = get_filepaths("/Users/allenfang/work/ScoopApp/app/assets/img/ServiceSelection/Background")
+print "\n".join(full_file_paths)
--- app/components/ServiceSelection/ServiceSelection.js
+++ app/components/ServiceSelection/ServiceSelection.js
@@ -4,15 +4,43 @@ import {
 	Text,
 	StyleSheet,
 	TouchableHighlight,
+	Image,
+	Dimensions,
+	ListView
 } from 'react-native';
 
+import images from '@assets/images';
+
 export default class ServiceSelection extends Component {
+	constructor() {
+		super();
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+
+		const row1 = {
+			imgUrl: images.airbnb_bgLogo
+		}
+		const row2 = {
+			imgUrl: images.audible_bgLogo
+		}
+
+
+		this.state = {
+			dataSource: ds.cloneWithRows([row1, row2]),
+		};
+	}
 	render() {
+		const width = Dimensions.get('window').width;
+		const height = 140
 		return(
 			<View>
 				<Text>
 					HelloWorld
 				</Text>
+				<ListView 
+					dataSource={this.state.dataSource}
+					renderRow={(rowData) => <Image source={rowData.imgUrl} style={{width: width, height: height}} />}
+				/>
 			</View>
 		)
 	}
